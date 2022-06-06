5bf143ecc9e039b4109d3c0123264c1a65a11cba
Lineisha <lineishagibson@gmail.com>
2022-06-06 15:24:52 -0500

    work

---
+++ src/ConsoleIO.java
@@ -0,0 +1,8 @@
+public class ConsoleIO {
+
+    public static void main(String[] args) {
+        String cohort = "Go";
+        String timeOfDay = "afternoon";
+        System.out.println("Hello, %s, %s!%n", timeOfDay , cohort);
+    }
+}
