8eefc229b409b358d70424e6332384997b8ca311
Lineisha <lineishagibson@gmail.com>
2022-06-07 15:39:54 -0500

    ib

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,11 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-int i = 5;
-while (i <= 15){
+int i = 0;
+do{
+//    System.out.println("value" + i);
     i = i + 1;
-    System.out.println(i);
-}
+    System.out.println("new value " + i);
+} while (i < 100);
 
     }
 }
