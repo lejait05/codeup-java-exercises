aa2000527ff643b7c7b653dae55417d3afa5e75c
Lineisha <lineishagibson@gmail.com>
2022-06-09 09:47:20 -0500

    work

---
+++ src/Bob.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        // ends with?
+        if (userInput.endsWith("?")){
+            System.out.println("Sure");
+            // ends with !
+        }else if ( userInput.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+            //empty string
+        }else if (userInput.trim().equals("")){
+            System.out.println("Fine. Be that way!");
+    }
+}
