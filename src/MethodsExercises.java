b2f505cd226bcaa42afed132144aca2365b05b69
Lineisha <lineishagibson@gmail.com>
2022-06-11 23:17:50 -0500

    work

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-    public static void main(String[] args) {
-
-        System.out.println(" Enter the numbers to be added:");
-        Scanner readInput = new Scanner(System.in);
-        int a = readInput.nextInt();
-        int b = readInput.nextInt();
-        readInput.close();
-        System.out.println("The sum of  a and b is :" + Integer.sum(a,b));
-
-
-
-        public static int getInteger(int min, int max);
-        if ( max == 0){
-            return 1;
-        }
-        System.out.print("Enter a number between 1 and 10: ");
-        int userInput = getInteger(1, 10);
-
-
-    }
-
+//    public static void main(String[] args) {
+//
+//        System.out.println(" Enter the numbers to be added:");
+//        Scanner readInput = new Scanner(System.in);
+//        int a = readInput.nextInt();
+//        int b = readInput.nextInt();
+//        readInput.close();
+//        System.out.println("The sum of  a and b is :" + Integer.sum(a,b));
+//
+//
+//
+//        public static int getInteger(int min, int max);
+//        if ( max == 0){
+//            return 1;
+//        }
+//        System.out.print("Enter a number between 1 and 10: ");
+//        int userInput = getInteger(1, 10);
+//
+//
+//    }
+//
 
 
 }
