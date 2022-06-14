592bd3964875a31761d1bdad80b5aa6bd89d7515
Lineisha <lineishagibson@gmail.com>
2022-06-14 10:26:40 -0500

    updated

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,5 +2,9 @@ public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(numbers);
+       String[] person = {"john", "jacob", "jingleheimerschmidt"};
+       for ( int i = 0; i < person.length; i += 1){
+           System.out.println(person[i]);
+       }
     }
 }
