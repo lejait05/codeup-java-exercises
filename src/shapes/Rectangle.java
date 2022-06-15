82e34f41d6897faaa791a5906f441a8d8b04a120
Lineisha <lineishagibson@gmail.com>
2022-06-15 14:38:43 -0500

    inherit

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,18 @@
 package shapes;
 
 public class Rectangle {
+  protected  length;
+  protected width;
+
+  public get getPerimeter() {
+    return perimeter;
+  }
+
+  perimeter = 2 x length + 2 x width
+
+  public get getArea() {
+    return area;
+  }
+
+  area = length x width
 }
