4625ab20f867a1c1331065ff71e7a71719e35c0a
Lineisha <lineishagibson@gmail.com>
2022-06-19 16:24:51 -0500

    assignment

---
+++ src/grades/Student.java
@@ -0,0 +1,23 @@
+package grades;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private String grades;
+    ArrayList<Integer> grade = new ArrayList<>();
+     grade.add();
+
+    // returns the student's name
+    public String getName(){return name;};
+    public void setName(String name){this.name = name;}
+    // adds the given grade to the grades property
+    public void addGrade(int grade);
+    // returns the average of the students grades
+    public double getGradeAverage();
+
+    public ArrayList<String> getGrades() {
+        return grades;
+    }
+}
